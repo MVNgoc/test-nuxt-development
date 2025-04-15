6a0ca1e14c9e8d137a674b1c1be8ed9550043fd8
maingoc <mvn,bp01@gmail.com>
2025-04-15 18:07:20 +0700

    rebuild

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,32 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
   // ssr: true,
   css: ["~/public/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
-
-  // app: {
-  //   head: {
-  //     title: "Yen Market",
-  //     meta: [
-  //       {
-  //         name: "description",
-  //         content:
-  //           "Đây là 1 trang hiển thị danh mục các sản phẩm của Yen Market",
-  //       },
-  //       {
-  //         name: "viewport",
-  //         content: "width=device-width, initial-scale=1",
-  //       },
-  //       {
-  //         charset: "utf-8",
-  //       },
-  //     ],
-  //   },
-  // },
-
   modules: ["@nuxt/image"],
 });
